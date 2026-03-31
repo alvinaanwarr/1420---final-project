7f32553f37838f716e89f64a2f43e27a7fd71d54
poetpeplum <emuusidd@gmail.com>
2026-03-31 14:08:34 -0400

    Delete src directory

--- src/model/BookingStatus.java
+++
@@ -1,7 +0,0 @@
-package model;
-
-public enum BookingStatus {
-    CONFIRMED,
-    WAITLISTED,
-    CANCELLED
-}
--- src/model/EventStatus.java
+++
@@ -1,9 +0,0 @@
-package model;
-
-/**
- * Represents the status of an event.
- */
-public enum EventStatus {
-    ACTIVE,
-    CANCELLED
-}
